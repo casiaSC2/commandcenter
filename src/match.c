1010a7dbedfd86ba5da105748f3776436e293880
Alchemist <yt4766269@126.com>
2017-11-03 16:52:56 +0800

    bot

--- src/match.c
+++ src/match.c
@@ -5,7 +5,7 @@
 JNIEXPORT jint JNICALL Java_ladder_service_MatchService_match
 (JNIEnv *env, jobject object, jstring botA_path, jstring botB_path, jstring map_path)
 {
-	char *sc2_path_str = "";
+	char *sc2_path_str = "D:\\game\\StarCraft II\\Versions\\Base58400\\SC2_x64.exe";
 	const char *botA_path_str = env->GetStringUTFChars(botA_path, 0);
 	const char *botB_path_str = env->GetStringUTFChars(botB_path, 0);
 	const char *map_path_str = env->GetStringUTFChars(map_path, 0);
